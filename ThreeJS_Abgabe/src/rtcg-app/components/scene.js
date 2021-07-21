4415b306da53de017fcb4cf1dc3c1bd3970b111b
AL <al@imsimity.de>
2021-07-21 18:09:19 +0200

    Update scene.js

--- ThreeJS_Abgabe/src/rtcg-app/components/scene.js
+++ ThreeJS_Abgabe/src/rtcg-app/components/scene.js
@@ -3,7 +3,7 @@ import { Scene, Color } from "https://unpkg.com/three@0.127.0/build/three.module
 function createScene() {
     const scene = new Scene();
 
-    scene.background = new Color("darkblue");
+    // scene.background = new Color("darkblue");
 
     return scene;
 }
